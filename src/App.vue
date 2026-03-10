2857f1e130cf9a8d97e1985f7d538113c3d2dd7e
PrtzArthur <prantsarthur2010@gmail.com>
2026-03-10 09:31:04 -0300

    Commited

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,55 @@
 <script setup>
-  import { ref } from 'vue'
-  const tarefas = ref(['Tarefa 1', 'Tarefa 2', 'Tarefa 3']);
+import { ref } from 'vue'
+const tarefas = ref(['Tarefa 1', 'Tarefa 2', 'Tarefa 3'])
 
-  const novaTarefa = ref('');
+const novaTarefa = ref('')
+const posAlterada = ref(-1)
 
-  function addTarefa() {
+function addTarefa() {
+  if (posAlterada.value == -1) {
     if (novaTarefa.value.trim().length >= 5) {
-      tarefas.value.push(novaTarefa.value);
+      tarefas.value.push(novaTarefa.value)
     }
-    novaTarefa.value = '';
+  }
+  else {
+    tarefas.value.splice(posAlterada, 1, novaTarefa.value);
+    posAlterada.value = -1;
   }
 
-  function delTarefa(item) {
-    const posicao = tarefas.value.indexOf(item);
-    tarefas.value.splice(posicao, 1)
-  }
+  novaTarefa.value = ''
+}
 
-  function ordenar() {
-    tarefas.value.sort();
-  }
+function delTarefa(item) {
+  const posicao = tarefas.value.indexOf(item)
+  tarefas.value.splice(posicao, 1)
+}
+
+function editTarefa(item) {
+  posAlterada = tarefas.value.indexOf(item)
+  novaTarefa.value = item;
+}
+
+function ordenar() {
+  tarefas.value.sort()
+}
 </script>
 
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
-    <input type="text" v-model="novaTarefa">
+    <input type="text" v-model="novaTarefa" />
     <button @click="addTarefa">Add</button>
     <ul>
       <li v-for="tarefa in tarefas" :key="tarefa">
         {{ tarefa }}
-        <span> 
+        <span>
           <a href="#" @click.prevent="editTarefa(tarefa)">Edit</a>
           <a href="#" @click.prevent="delTarefa(tarefa)">Delete</a>
         </span>
-      </li> 
+      </li>
     </ul>
     <button @click="ordenar">Ordenar</button>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
