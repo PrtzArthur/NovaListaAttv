7b02cfd8e1191a85e78ea852c8bf535d7e73ad66
Arthur-Prants <prantsarthur@gmail.com>
2026-03-16 20:05:31 -0300

    Perfecto

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ function addTarefa() {
     }
   }
   else {
-    tarefas.value.splice(posAlterada, 1, novaTarefa.value);
+    tarefas.value.splice(posAlterada.value, 1, novaTarefa.value);
     posAlterada.value = -1;
   }
 
@@ -25,13 +25,20 @@ function delTarefa(item) {
 }
 
 function editTarefa(item) {
-  posAlterada = tarefas.value.indexOf(item)
+  posAlterada.value = tarefas.value.indexOf(item)
   novaTarefa.value = item;
 }
 
 function ordenar() {
   tarefas.value.sort()
 }
+
+function concluirTarefa(item) {
+  const posicao = tarefas.value.indexOf(item)
+  const lista = document.querySelectorAll(".texto")
+  lista[posicao].classList.add("concluida")
+}
+
 </script>
 
 <template>
@@ -41,10 +48,11 @@ function ordenar() {
     <button @click="addTarefa">Add</button>
     <ul>
       <li v-for="tarefa in tarefas" :key="tarefa">
-        {{ tarefa }}
+        <span class="texto">{{ tarefa }}</span>
         <span>
           <a href="#" @click.prevent="editTarefa(tarefa)">Edit</a>
           <a href="#" @click.prevent="delTarefa(tarefa)">Delete</a>
+          <a href="#" @click.prevent="concluirTarefa(tarefa)">Concluir</a>
         </span>
       </li>
     </ul>
@@ -52,4 +60,16 @@ function ordenar() {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div.container ul li span a {
+  color: brown;
+  margin-left: 10px;
+}
+
+.concluida {
+  text-decoration: line-through;
+  color: gray;
+  opacity: 0.6;
+}
+
+</style>
